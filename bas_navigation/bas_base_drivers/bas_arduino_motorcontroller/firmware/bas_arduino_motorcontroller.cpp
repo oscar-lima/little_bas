9e803077d37f2ef950dded38ea68953fb5f719a0
Oscar Lima <olima_84@yahoo.com>
2015-01-22 23:57:15 +0100

    renaming from bas_motor_driver to bas_arduino_motorcontroller

---
+++ bas_navigation/bas_base_drivers/bas_arduino_motorcontroller/firmware/bas_arduino_motorcontroller.cpp
@@ -0,0 +1,27 @@
+#include <ros.h>
+#include <std_msgs/String.h>
+
+#include <Arduino.h>
+
+ros::NodeHandle nh;
+
+std_msgs::String str_msg;
+ros::Publisher chatter("chatter", &str_msg);
+
+char hello[13] = "hello world!";
+
+void setup()
+{
+  nh.initNode();
+  nh.advertise(chatter);
+}
+
+void loop()
+{
+  str_msg.data = hello;
+  chatter.publish( &str_msg );
+  nh.spinOnce();
+  delay(1000);
+}
+
+//this is just a template!!!!, fill in with bas driver needs to be still done!!
